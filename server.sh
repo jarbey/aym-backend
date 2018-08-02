eeba02c98ba452be73a4d89905db35d7826a8799
Julien ARBEY <julien@arbey.fr>
2018-08-02 18:50:17 +0200

    PHP 7.1 req

---
+++ server.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
+
+export $(cat $BASEDIR/.env | grep -v ^# | xargs)
+
+/usr/bin/php $BASEDIR/bin/console aym:websocket:server -vv 2>&1 >> $BASEDIR/websocket.log
